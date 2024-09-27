9848d37228954fbf17866f6adcc0fb2cc93fba0a
Mayilerumperumal <mayilerumperumal@blindmatrix.co.uk>
2024-09-27 20:29:15 +0530

    design1

--- src/Core/Components/Header/index.tsx
+++ src/Core/Components/Header/index.tsx
@@ -4,8 +4,10 @@ import './HeaderStyle.css'
 const Header:FC<HeaderProps>=()=>{
     return(
 <div className='Container' >
-   <h1>llllll</h1>
-{/* <span te='Wix' style={{fontSize:12,color:'red'}}/> */}
+ <a href=''  className='titleContainer'>
+ <span  className='title'></span>
+
+ </a>
 
 </div>
     )

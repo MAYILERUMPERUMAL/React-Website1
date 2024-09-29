aedb9ab2f3322b1f2d844da9571b8911267b8b84
MAYILERUMPERUMAL <shiyamraju1999@gmail.com>
2024-09-29 23:21:24 +0530

    chnages-design2

--- src/Core/Components/Header/index.tsx
+++ src/Core/Components/Header/index.tsx
@@ -3,12 +3,31 @@ import {HeaderProps} from './interface'
 import './HeaderStyle.css'
 const Header:FC<HeaderProps>=()=>{
     return(
-<div className='Container' >
- <a href=''  className='titleContainer'>
- <span  className='title'></span>
+<div className='headerContainer' >
+ {/* <a href=''  className='titleContainer'> */}
+ <div className='websiteTitle'>
+ <h1>Wix</h1>
 
- </a>
+ </div>
 
+ {/* </a> */}
+ <div className='deviceOptionContainer'>
+ <div >
+ <img className='imgConatiner' src='https://www.svgrepo.com/show/533130/monitor.svg' style={{fill:'black'}} alt='black'/>
+ <img className='imgConatiner' src='https://www.svgrepo.com/show/499623/mobile.svg' style={{fill:'black'}}/>
+ </div>
+
+<div>
+    <p>Click edit and create your own amazing website</p>
+</div>
+<div>
+    <a>Read More</a>
+</div>
+
+<div className='PrimaryButton'>
+    <label className='PrimaryButtonLabel'> Edit  this site</label>
+   </div>
+   </div>
 </div>
     )
 }
--- src/Modules/Screen/HomeScreen/index.tsx
+++ src/Modules/Screen/HomeScreen/index.tsx
@@ -1,10 +1,44 @@
 import React,{FC} from 'react'
 import {HomeScreenProps} from './interface'
 import { Header } from '../../../Core'
+import ScrollAnimation from 'react-animate-on-scroll';
+import './HomeStyle.css'
 const HomeScreen:FC<HomeScreenProps>=()=>{
     return(
-<div >
+<div className='Container' >
 <Header/>
+<div className='OptionListHeader'>
+    <div className='optionlistContainer1'>
+
+  
+<div className='optionlist1'>
+    <label className='optionlist1Text1'>BRW</label>
+    <label className='optionlist1Text2'>BAR INC @</label>
+
+</div>
+</div>
+<div className='optionlistContainer2'>
+    <div className='optionlistContainer2TextBox'>
+        <label className='optionlistContainer2TextBoxText'>ABOUT</label>
+    </div>
+    <div className='optionlistContainer2TextBox'>
+        <label className='optionlistContainer2TextBoxText'>TREATMENTS</label>
+    </div>
+    <div className='optionlistContainer2TextBox'>
+        <label className='optionlistContainer2TextBoxText'>PACKAGES</label>
+    </div>
+    <div className='optionlistContainer2TextBox'>
+        <label className='optionlistContainer2TextBoxText'>CONTACT</label>
+    </div>
+</div>
+<div className='loginandcartContainer'>
+    <div className='LoginContainer' onChange={()=>{}}>
+        <label className='loginTextStyle'>Login</label>
+    </div>
+    <img src='https://www.svgrepo.com/show/505591/cart-1.svg' className='cartImageStyle'/>
+</div>
+</div>
+
 </div>
     )
 }
